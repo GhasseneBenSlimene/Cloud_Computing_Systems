d22394cd832e25d709e76dbc779247d8fd199e6f
Ghassene Ben Slimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2024-10-09 17:52:30 +0200

    Exercice 3 : user defined network

--- api/server.mjs
+++ api/server.mjs
@@ -3,7 +3,7 @@ import Fastify from "fastify";
 export function createServer() {
   const fastify = Fastify();
 
-  fastify.get("/feature/:code/", async function handler(request, reply) {
+  fastify.get("/feature/:code", async function handler(request, reply) {
     const code = request.params.code
     reply.send({'code':code, 'enabled':true})
   });
